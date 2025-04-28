73e9ae578f2a05d26f30ed35ab5eb09405492c3b
AnujNigam12 <anujnig.07@gmail.com>
2025-04-28 10:26:37 +0530

    Version3

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectedToDB = async () => {
     try {
-        // let data =  await mongoose.connect(process.env.MONGO_URI)
-        let data =  await mongoose.connect("mongodb://localhost:27017/socialMedia")
+        let data =  await mongoose.connect(process.env.MONGO_URI)
+        // let data =  await mongoose.connect("mongodb://localhost:27017/socialMedia")
         console.log("Connected to Database!")
     } catch (error) {
         console.log("Error to connect to Database", error)
